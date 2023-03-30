2fb4d238a4f5b78cc75b97b840851e19d647e63c
Vadim <vadik-izum@tut.by>
2023-03-30 23:49:15 +0300

    Добавил эталонные решения в задачи

--- Zadacha_24.py
+++ Zadacha_24.py
@@ -35,3 +35,17 @@ def max_berry(N):
 
 N = int(input("Введите количество кустов: "))
 max_berry(N)
+
+
+# Эталонное решение
+# n = int(input())
+# arr = list()
+# for i in range(n):
+#     x = int(input())
+#     arr.append(x)
+
+# arr_count = list()
+# for i in range(len(arr) - 1):
+#     arr_count.append(arr[i - 1] + arr[i] + arr [i + 1])
+# arr_count.append(arr[-2] + arr[-1] + arr[0])
+# print(max(arr_count))
