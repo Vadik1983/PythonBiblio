3ed92cc54787cb4e19dcbe13b78f42d4f2c15d1a
VadimSid <119668494+Vadik1983@users.noreply.github.com>
2025-04-04 10:01:30 +0300

    исправлено

--- Zadacha_2.py
+++ Zadacha_2.py
@@ -1,8 +1,8 @@
-#Задача 2: Найдите сумму цифр трехзначного числа.
+234#Задача 2: Найдите сумму цифр трехзначного числа.
 
 number = int(input('Введите число: '))
 sum = 0
-
+if number < 0 : number = number *(-1)
 while number > 0:
     digit = (number % 10)
     sum = sum + digit
